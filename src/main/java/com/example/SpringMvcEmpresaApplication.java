f93693fa1b64f018e8e9393e5d829bee1663bd2a
mromerod <maramanss99@gmail.com>
2023-03-23 18:02:42 +0100

    rama main con entities creadas

---
+++ src/main/java/com/example/SpringMvcEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcEmpresaApplication.class, args);
+	}
+
+}
